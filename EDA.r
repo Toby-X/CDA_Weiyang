0942c20698e448d19e8b65ed1d1f4ac2256c9869
Toby-X <751280216@qq.com>
2022-12-04 16:46:37 +0800

    normalized data and boxplots

--- EDA.r
+++ EDA.r
@@ -1,1 +1,11 @@
 #-*- coding: utf-8 -*-
+library(GGally)
+library(tidyverse)
+
+data = read.csv("./Datasets/bilibili.csv",header=T)
+colnames(data)[1] = "idx"
+
+## Create Scatter Plot Matrix
+ggpairs(data[,c(3:10,13)])#use av_coin av_like作为代表画spm
+corr = cor(data[,-1:-2])
+heatmap(corr,Colv = NA,Rowv=NA,scale = "column")
